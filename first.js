3b420756241097cb8f4604f46a0c07c4b2ede741
Vishal Bisht <vishalbisht987@gmail.com>
2025-01-19 22:06:38 +0530

    payment updated after footer

--- first.js
+++ first.js
@@ -4,6 +4,14 @@ console.log(button);
 const login="Let's Login";
 console.log(login);
 
+<<<<<<< HEAD
 const footer="It's a footer";
 console.log(footer);
+=======
+const pay="Let's pay";
+console.log(pay);
+
+const upi="UPI added";
+console.log(upi);
+>>>>>>> ab49335 (pay and UPI)
 
