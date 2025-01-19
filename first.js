19eb32138d51fd8ab5ad3169f9a26b65c4d3a523
Vishal Bisht <vishalbisht987@gmail.com>
2025-01-19 22:46:06 +0530

    New

--- first.js
+++ first.js
@@ -4,14 +4,12 @@ console.log(button);
 const login="Let's Login";
 console.log(login);
 
-<<<<<<< HEAD
 const footer="It's a footer";
 console.log(footer);
-=======
 const pay="Let's pay";
 console.log(pay);
 
 const upi="UPI added";
 console.log(upi);
->>>>>>> ab49335 (pay and UPI)
 
+console.log("Latest update");
